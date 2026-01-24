18c8f1e9ba1610e2e2a3af4ed64f651a5a6d4b48
thrishashetty <shettythrisha08@gmail.com>
2026-01-24 15:08:34 +0530

    adding new file

--- python2.py
+++ python2.py
@@ -1,1 +1,35 @@
-bitch=1
+#concession stand program
+menu={"pizza":3.00,
+      "nachos":4.50,
+      "popcorn":6.00,
+      "fries":1.00,
+      "chips":2.50,
+      "pretzel":3.50,
+      "soda":3.00,
+      "lemonade":4.25}
+
+
+cart=[]
+total=0
+
+print("----------MENU----------")
+for key,value in menu.items():
+    print(f"{key:10}:${value:.2f}")
+print("------------------------")
+
+while True:
+    food=input("enter the types you wanna buy(q to quit)").lower()
+    if food== "q":
+        break
+    elif menu.get(food) is not None:
+        cart.append(food)
+
+
+for food in cart:
+    total+=menu.get(food)
+    print(food,end=" ")
+
+print()
+print(f"total is :${total}")
+
+
